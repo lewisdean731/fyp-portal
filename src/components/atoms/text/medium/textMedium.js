c863499432a64345d2bdf8500e67970c00d7bd93
Lewis Dean <lewisdean731@gmail.com>
2021-03-12 17:21:39 +0000

    feat: add medium text atom
    [Ticket: FYP-5]

---
+++ src/components/atoms/text/medium/textMedium.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Classes from "./textMedium.module.scss";
+
+function TextMedium(props) {
+  return (
+    <p className={`${Classes.textMedium} ${Classes[props.colour]}`}>{props.children}</p>
+  );
+}
+
+export default TextMedium;
