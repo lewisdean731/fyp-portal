c55d28eec61e80438fb6658878893aef7abbba49
Lewis Dean <lewisdean731@gmail.com>
2021-03-13 16:55:06 +0000

    Feat: add CSS classes to dashboardItem
    [Ticket: FYP-5]

--- src/components/molecules/ui/dashboardItem/dashboardItem.js
+++ src/components/molecules/ui/dashboardItem/dashboardItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 import TextLarge from "../../../atoms/text/large/textLarge";
 import TextMedium from "../../../atoms/text/medium/textMedium";
+import Classes from "./dashboardItem.module.scss";
 
 function DashboardItem(props) {
   return (
-    <div>
+    <div className={Classes.dashboardItem}>
       <TextLarge colour={props.colour1}>{props.text1}</TextLarge>
       <TextMedium colour={props.colour2}>{props.text2}</TextMedium>
     </div>
