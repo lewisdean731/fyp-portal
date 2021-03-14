57153a45e1edaa5fc70c0a62f9f3d1b2b0bd1335
Lewis Dean <lewisdean731@gmail.com>
2021-03-14 14:33:27 +0000

    chore: more styling for dashboardNotificationItem
    [Ticket: FYP-5]

--- src/components/molecules/ui/dashboardNotificationItem/dashboardNotificationItem.js
+++ src/components/molecules/ui/dashboardNotificationItem/dashboardNotificationItem.js
@@ -7,7 +7,7 @@ function dashboardNotificationItem(props) {
   return (
     <Row className={"justify-content-md-center"}>
       <Col md={8} className={Classes.dashboardNotificationItem}>
-        <TextSmall colour={'grey'}><img src="https://via.placeholder.com/48.png" />{props.textDetails}</TextSmall>      
+        <TextSmall colour={'grey'}><img src="https://via.placeholder.com/48.png" />&emsp;{props.textDetails}</TextSmall>      
       </Col>
     </Row>
   );
