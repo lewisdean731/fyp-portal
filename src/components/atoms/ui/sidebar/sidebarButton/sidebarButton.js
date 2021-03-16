a940c85314e41e289ce102052923d7c03ce1f1d6
Lewis Dean <lewisdean731@gmail.com>
2021-03-16 15:27:43 +0000

    feat: implement sidebar button atom
    [Ticket: FYP-6]

---
+++ src/components/atoms/ui/sidebar/sidebarButton/sidebarButton.js
@@ -0,0 +1,8 @@
+import {Button} from "react-bootstrap";
+import Classes from "./sidebarButton.module.scss";
+
+export default function SidebarButton(props) {
+  return (
+    <Button href={props.link} className={Classes.button}>{props.children}</Button>
+  )
+}
