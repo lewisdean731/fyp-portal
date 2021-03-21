6ec43b6c54a260183e9aeba6b43d81814222b980
Lewis Dean <lewisdean731@gmail.com>
2021-03-21 18:35:16 +0000

    feat: add logo to topbar
    [Ticket: FYP-6]

--- src/components/organisms/ui/topbar/topbar.js
+++ src/components/organisms/ui/topbar/topbar.js
@@ -1,8 +1,11 @@
 import {Row, Col} from "react-bootstrap";
+import TextMedium from "../../../atoms/text/medium/textMedium";
 import Classes from "./topbar.module.scss";
 export default function Topbar(props){
   return(
       <div className={Classes.topbar}>
+        <img className={Classes.logo} src="../../logo200.png"/>
+        <TextMedium className={Classes.logoText} colour={"grey"}>MonTor</TextMedium>
         <div className={Classes.border}></div>
       </div>
 
