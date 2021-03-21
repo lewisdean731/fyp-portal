b56866bc10bb60d562ed0109162cffe1f6e949a9
Lewis Dean <lewisdean731@gmail.com>
2021-03-21 17:51:36 +0000

    feat: implement sidebar organism
    and add it to App.js
    
    [Ticket: FYP-6]

---
+++ src/components/organisms/ui/sidebar/sidebar.js
@@ -0,0 +1,14 @@
+import {Row, Col} from "react-bootstrap";
+import SidebarButtons from "../../../molecules/ui/sidebarButtons/sidebarButtons";
+import Classes from "./sidebar.module.scss";
+export default function Sidebar(props){
+  return(
+    <div className={Classes.sidebar}>
+      <div className={Classes.padding64}></div>
+      <div>Sidebar / Popout Nav Menu</div>   
+      <div className={Classes.padding24}></div>
+      <SidebarButtons />
+      <div className={Classes.border}></div>
+    </div>
+  )
+}
