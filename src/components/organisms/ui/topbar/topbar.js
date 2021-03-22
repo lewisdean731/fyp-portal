3ca40c0d48ea066c6aaa5585f43de69f0a89fac6
Lewis Dean <lewisdean731@gmail.com>
2021-03-22 00:31:24 +0000

    feat: add search bar and placeholder username
    to the topbar
    
    [Ticket: FYP-6]

--- src/components/organisms/ui/topbar/topbar.js
+++ src/components/organisms/ui/topbar/topbar.js
@@ -1,13 +1,17 @@
-import {Row, Col} from "react-bootstrap";
+import {Row} from "react-bootstrap";
 import TextMedium from "../../../atoms/text/medium/textMedium";
+import TextSmall from "../../../atoms/text/small/textSmall";
+import SearchTopbar from "../../../atoms/ui/search/topbar/searchTopbar";
 import Classes from "./topbar.module.scss";
 export default function Topbar(props){
   return(
-      <div className={Classes.topbar}>
+      <Row className={Classes.topbar}>
         <img className={Classes.logo} src="../../logo200.png"/>
-        <TextMedium className={Classes.logoText} colour={"grey"}>MonTor</TextMedium>
+        <TextMedium className={Classes.logoText} colour={"grey"}>MoniTor</TextMedium>
+        <TextSmall className={Classes.username} colour={"grey"}>Username</TextSmall>
+        <SearchTopbar className={Classes.searchbar}/>
         <div className={Classes.border}></div>
-      </div>
+      </Row>
 
   )
 }
