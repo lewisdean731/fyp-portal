02b4faa03aa2ceccaa50750f5057659412d45dec
Lewis Dean <lewisdean731@gmail.com>
2021-05-06 16:52:24 +0100

    feat: remove authentication from routes.js
    It is all handled in app.js now. Additionally, the login page is rendered
    by app.js and does not rely on routes.
    
    [Ticket: FYP-13]

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ import Classes from "./App.module.scss";
 import Sidebar from "./components/organisms/ui/sidebar/sidebar";
 import Topbar from "./components/organisms/ui/topbar/topbar";
 
-import ROUTES, { RenderRoutes } from "./routes";
+import Login from "./layouts/login/login";
+import ROUTES, { RenderRoutes,  } from "./routes";
+
 
 function App() {
   return (
     <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
       <IfFirebaseUnAuthed>
         {() => {
-          return <RenderRoutes routes={ROUTES} />;
+          return <Login />;
         }}
       </IfFirebaseUnAuthed>
       <IfFirebaseAuthed>
