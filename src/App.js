d8b9b57a25518b90074903998b55134a2ca4b896
Lewis Dean <lewisdean731@gmail.com>
2021-05-06 16:18:01 +0100

    feat: implement firebase login
    entering correct username-password now signs the user in using firebase
    
    incldued some debug buttons to easily test sign-in and out.
    
    [Ticket: FYP-13]

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase/firebaseConfig";
+
+import {
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseUnAuthed,
+} from "@react-firebase/auth";
+
 import { Container, Row, Col } from "react-bootstrap";
 import Classes from "./App.module.scss";
 import Sidebar from "./components/organisms/ui/sidebar/sidebar";
@@ -7,21 +17,50 @@ import ROUTES, { RenderRoutes } from "./routes";
 
 function App() {
   return (
-    <Container fluid className={Classes.app}>
-      <Row>
-        <Col className={Classes.topbar}>
-          <Topbar />
-        </Col>
-      </Row>
-      <Row>
-        <Col className={Classes.sidebar}>
-          <Sidebar />
-        </Col>
-        <Col>
-          <RenderRoutes routes={ROUTES} />
-        </Col>
-      </Row>
-    </Container>
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <IfFirebaseUnAuthed>
+        {() => {
+          return <RenderRoutes routes={ROUTES} />;
+        }}
+      </IfFirebaseUnAuthed>
+      <IfFirebaseAuthed>
+        {() => {
+          return (
+            <Container fluid className={Classes.app}>
+              <Row>
+                <Col className={Classes.topbar}>
+                  <Topbar />
+                </Col>
+              </Row>
+              <Row>
+                <Col className={Classes.sidebar}>
+                  <Sidebar />
+                </Col>
+                <Col>
+                  <RenderRoutes routes={ROUTES} />
+                </Col>
+              </Row>
+            </Container>
+          );
+        }}
+      </IfFirebaseAuthed>
+      <div className={Classes.debugAuthButtons}>
+        <button
+          onClick={() => {
+            firebase.auth().signOut();
+          }}
+        >
+          Sign Out
+        </button>
+        <button
+          onClick={() => {
+            firebase.auth().signInAnonymously();
+          }}
+        >
+          Sign In Anon.
+        </button>
+      </div>
+    </FirebaseAuthProvider>
   );
 }
 
--- src/components/molecules/ui/login/usernamePassword/usernamePassword.js
+++ src/components/molecules/ui/login/usernamePassword/usernamePassword.js
@@ -1,20 +1,40 @@
+import React, { useState } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
+import firebase from "firebase/app";
 import Classes from "./usernamePassword.module.scss";
 
 export default function LoginUsernamePassword(props) {
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
   return (
     <Row className={`${Classes.login} ${props.className}`}>
-      <Col noGutters>
+      <Col>
         <Form className={Classes.form}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label srOnly>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(event) => setSignInEmail(event.target.value)}
+            />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label srOnly>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(event) => setSignInPassword(event.target.value)}
+            />
           </Form.Group>
-          <Button variant="success" type="submit" className={Classes.wide}>
+          <Button
+            variant="success"
+            className={Classes.wide}
+            onClick={() => {
+              firebase
+                .auth()
+                .signInWithEmailAndPassword(signInEmail, signInPassword);
+            }}
+          >
             Log In
           </Button>
           <hr />
