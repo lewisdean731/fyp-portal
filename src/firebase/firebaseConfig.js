f63955944159740aa344e7cd1e67a1f79159ff73
Lewis Dean <lewisdean731@gmail.com>
2021-05-06 15:42:05 +0100

    feat: create importable firebaseconfig
    [Ticket: FYP-13]

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8bNNfasp-9h7EY2VFm0BtCuPD3uLOAHs",
+  authDomain: "bu-fyp-s5008913.firebaseapp.com",
+  projectId: "bu-fyp-s5008913",
+  storageBucket: "bu-fyp-s5008913.appspot.com",
+  messagingSenderId: "573526317900",
+  appId: "1:573526317900:web:a1adbdef37128f32bbcde9",
+  measurementId: "G-90TFM0FYDH"
+};
+
+export default firebaseConfig;
