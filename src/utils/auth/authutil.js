d17f09061877777c0ff0299832f6e8d18a8b20b8
Lewis Dean <lewisdean731@gmail.com>
2021-05-08 23:27:51 +0100

    feat: pass token as authorisation on API requests
    all API requests now require authentication in the form of the current user's
    access token.

--- src/utils/auth/authutil.js
+++ src/utils/auth/authutil.js
@@ -3,5 +3,5 @@ import {asyncGetRequest} from "../apiUtil";
 export const verifyToken = async (token) => {
   console.log("Checking login status..");
   return await asyncGetRequest(`/api/auth/verifytoken?token=${token}
-  `);
+  `, token);
 };
