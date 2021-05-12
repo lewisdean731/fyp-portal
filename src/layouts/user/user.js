550180abf83ac978b9b6195015b7873d3b1a6a3d
Lewis Dean <lewisdean731@gmail.com>
2021-05-13 00:54:59 +0100

    feat: fetch user firestore data from API
    [Ticket: FYP-16]

--- src/layouts/user/user.js
+++ src/layouts/user/user.js
@@ -1,12 +1,22 @@
+import { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import TextLarge from "../../components/atoms/text/large/textLarge";
 import UserDetails from "../../components/organisms/ui/user/userDetails/userDetails";
+import { getUserFirestoreInformation } from "../../utils/apiUtil";
 
 function User(props) {
+  
+  const [userFirestoreData, setUserFirestoreData] = useState("");
+
+  useEffect(async () => {
+    const data = await getUserFirestoreInformation(props.userData.user["uid"], props.userData.user.stsTokenManager["accessToken"])
+    setUserFirestoreData(data)
+  });
+
   return (
     <div>
       <TextLarge>My Account</TextLarge>
-      <UserDetails userData={props.userData} />
+      <UserDetails userData={props.userData} userFirestoreData={userFirestoreData} />
     </div>
   );
 }
