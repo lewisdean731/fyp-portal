531f2a159ddf2e32f707e68e40f38ca304d74b49
Lewis Dean <lewisdean731@gmail.com>
2021-05-13 00:14:06 +0100

    feat: implement userDetails organism
    [Ticket: FYP-16]

---
+++ src/components/organisms/ui/user/userDetails/userDetails.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import TextMedium from "../../../../atoms/text/medium/textMedium";
+import UserDetailsForm from "../../../../molecules/ui/userDetailsForm/userDetailsForm";
+
+import Classes from "./userDetails.module.scss";
+
+function UserDetails(props) {
+  return (
+    <Container fluid className={Classes.userDetails}>
+      <TextMedium colour={"grey"}>Personal Details</TextMedium>
+      <Row className="justify-content-md-center">
+        <Col>
+          <UserDetailsForm userData={props.userData} />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default UserDetails;
