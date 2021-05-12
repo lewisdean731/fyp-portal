bff52cab462030f49df8f613c0762eb96aa0301e
Lewis Dean <lewisdean731@gmail.com>
2021-05-13 00:02:01 +0100

    feat: create form for updating user details
    readonly at the moment - functionality will come later but it is
    prepopulated with user details already
    
    [Ticket: FYP-16]

---
+++ src/components/molecules/ui/userDetailsForm/userDetailsForm.js
@@ -0,0 +1,60 @@
+import { Col, Form, Button } from "react-bootstrap";
+import Classes from "./userDetailsForm.module.scss";
+
+function UserDetailsForm(props) { 
+  return(
+    <div>
+      <Form className={Classes.personalDetails}>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formPlaintextPersonalDetails01">
+            <Form.Label>Name</Form.Label>
+            <Form.Control readOnly defaultValue={props.userData.user["displayName"]} />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formPlaintextPersonalDetails02">
+            <Form.Label>Telephone</Form.Label>
+            <Form.Control readOnly defaultValue={props.userData.user["phoneNumber"]} />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>    
+          <Form.Group as={Col} controlId="formPlaintextPersonalDetails03">
+            <Form.Label>Email</Form.Label>
+            <Form.Control readOnly defaultValue={props.userData.user["email"]} />
+          </Form.Group>
+
+          <Col />    
+
+        </Form.Row>
+        <br />
+        <Form.Row> 
+          <Form.Group as={Col} controlId="formPlaintextSubmitButton">   
+            <Button variant="primary" type="submit">
+                  Update Details
+            </Button>
+          </Form.Group>
+        </Form.Row>
+        
+      </Form>
+
+      <hr />
+
+      <Form className={Classes.updatePassword}>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formPlaintextPassword">
+            <Form.Label>Update Password</Form.Label>
+              <Form.Control readOnly type="password" placeholder="Old Password" />
+              <br />
+              <Form.Control readOnly type="password" placeholder="New Password" />
+              <br />
+              <Form.Control readOnly type="password" placeholder="Confirm Password" />
+              <br />
+              <Button variant="primary" type="submit">
+                Update Password
+              </Button>
+          </Form.Group>
+        </Form.Row>
+      </Form>
+    </div>
+  )
+}
+
+export default UserDetailsForm
