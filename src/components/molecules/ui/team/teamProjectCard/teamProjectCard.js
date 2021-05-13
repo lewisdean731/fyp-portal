e20d9c5adf903fa946858e79e0e610e07c1517ab
Lewis Dean <lewisdean731@gmail.com>
2021-05-13 17:34:13 +0100

    feat: implement teamproject molecule card
    [Ticket: FYP-23]

---
+++ src/components/molecules/ui/team/teamProjectCard/teamProjectCard.js
@@ -0,0 +1,16 @@
+import { Card, Button } from "react-bootstrap";
+
+function TeamProjectCard(props) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>Type: {props.projectType}</Card.Text>
+        <Card.Text>ID: {props.id}</Card.Text>
+        <Button variant="primary">Go to Project Page</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default TeamProjectCard;
