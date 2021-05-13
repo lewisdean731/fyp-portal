b8b9026408cfd6fe2c643b3e850a60bc10e75487
Lewis Dean <lewisdean731@gmail.com>
2021-05-13 14:07:24 +0100

    chore: move userPasswordForm to own component
    [Ticket: FYP-16]

--- src/components/molecules/ui/user/userDetailsForm/userDetailsForm.js
+++ src/components/molecules/ui/user/userDetailsForm/userDetailsForm.js
@@ -34,25 +34,6 @@ function UserDetailsForm(props) {
         </Form.Row>
         
       </Form>
-
-      <hr />
-
-      <Form className={Classes.updatePassword}>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Label>Update Password</Form.Label>
-              <Form.Control readOnly type="password" placeholder="Old Password" />
-              <br />
-              <Form.Control readOnly type="password" placeholder="New Password" />
-              <br />
-              <Form.Control readOnly type="password" placeholder="Confirm Password" />
-              <br />
-              <Button variant="primary" type="submit">
-                Update Password
-              </Button>
-          </Form.Group>
-        </Form.Row>
-      </Form>
     </div>
   )
 }
--- src/components/organisms/ui/user/userDetails/userDetails.js
+++ src/components/organisms/ui/user/userDetails/userDetails.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import TextMedium from "../../../../atoms/text/medium/textMedium";
-import UserDetailsForm from "../../../../molecules/ui/userDetailsForm/userDetailsForm";
+import UserDetailsForm from "../../../../molecules/ui/user/userDetailsForm/userDetailsForm";
+import UserPasswordForm from "../../../../molecules/ui/user/userPasswordForm/userPasswordForm";
 
 import Classes from "./userDetails.module.scss";
 
@@ -12,6 +13,8 @@ function UserDetails(props) {
       <Row className="justify-content-md-center">
         <Col>
           <UserDetailsForm userData={props.userData} />
+          <hr />
+          <UserPasswordForm userData={props.userData} />
         </Col>
       </Row>
     </Container>
