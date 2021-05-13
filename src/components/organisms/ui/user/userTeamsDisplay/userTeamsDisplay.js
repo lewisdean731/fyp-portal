858f6777b11211cf5b5a475bb5e8d8f6d3eb5e63
Lewis Dean <lewisdean731@gmail.com>
2021-05-13 16:34:04 +0100

    feat: group cards in seperate rows
    [Ticket: FYP-16]

--- src/components/organisms/ui/user/userTeamsDisplay/userTeamsDisplay.js
+++ src/components/organisms/ui/user/userTeamsDisplay/userTeamsDisplay.js
@@ -1,14 +1,34 @@
-import { Button, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import UserTeamCard from "../../../../molecules/ui/user/userTeamCard/userTeamCard";
+import { createRowArray } from "../../../../../utils/gridUtil";
 
 function UserTeamsDisplay(props) { 
 
   const teams = props.teams?props.teams:["No Team Data Available"] //Handle no teams in props
+
+  const grid = createRowArray(4, teams)
+
+  //The two maps create a set of rows and columns containing an arbtrary no. of 
+  //Cards
+
   return(
     <div>
-      {teams.map((id) => (
-        <UserTeamCard id={id} />
-      ))}
+      {grid.map((x, row) => {
+        return (
+          <div>
+            <Row className="justify-content-md-center">
+              {grid[row].map((index) => {
+                return (
+                  <Col md={3}>
+                    <UserTeamCard id={teams[index]} />
+                  </Col> 
+                )
+              })}
+            </Row>
+            <br />  
+          </div>
+        )
+      })}
     </div>
   )
 }
