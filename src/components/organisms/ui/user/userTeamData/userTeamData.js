a1df6b9b8bc39b2f03c907d6448451a573076996
Lewis Dean <lewisdean731@gmail.com>
2021-05-14 13:33:50 +0100

    feat: move logic for getting team information to userTeamsData
    [Ticket: FYP-23]

--- src/components/organisms/ui/user/userTeamData/userTeamData.js
+++ src/components/organisms/ui/user/userTeamData/userTeamData.js
@@ -1,17 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import TextMedium from "../../../../atoms/text/medium/textMedium";
 import UserTeamsDisplay from "../../../../molecules/ui/user/userTeamsDisplay/userTeamsDisplay";
-
+import { 
+  getUserFirestoreInformation, 
+  getTeamFirestoreInformation 
+} from "../../../../../utils/apiUtil"
 import Classes from "./userTeamData.module.scss";
 
 function UserTeamData(props) {
+  const [teamsData, setTeamsData] = useState([]);
+
+  // Get user data, then get team data for all user's teams
+  useEffect(async () => {
+    await getUserFirestoreInformation(
+      props.uid,
+      props.token
+    )
+      .then(async (data) => {
+        console.log(data);
+        return data;
+      })
+      .then((data) => {
+        data.teams.map(async (teamId) => {
+          await getTeamFirestoreInformation(
+            teamId,
+            props.token
+          ).then((data) => {
+            data["teamId"] = teamId;
+            console.log(data)
+            setTeamsData((teamsData) => [...teamsData, data]);
+          });
+        });
+      });
+  }, []);
+
   return (
     <Container fluid className={Classes.userTeamData}>
       <TextMedium colour={"grey"}>Teams</TextMedium>
       <Row className="justify-content-md-center">
         <Col>
-          <UserTeamsDisplay teams={props.userFirestoreData.teams} />
+          <UserTeamsDisplay teamsData={teamsData}/>
         </Col>
       </Row>
     </Container>
--- src/layouts/team/teams.js
+++ src/layouts/team/teams.js
@@ -1,45 +1,14 @@
-import { useState, useEffect } from "react";
 import TextLarge from "../../components/atoms/text/large/textLarge";
 import CreateTeam from "../../components/organisms/ui/team/createTeam/createTeam";
 import UserTeamData from "../../components/organisms/ui/user/userTeamData/userTeamData";
-import {
-  getTeamFirestoreInformation,
-  getUserFirestoreInformation,
-} from "../../utils/apiUtil";
 function Teams(props) {
-  const [teamsData, setTeamsData] = useState("");
-  const [userFirestoreData, setUserFirestoreData] = useState("");
-
-  // Get team data, then get project data for projects owned by team
-  useEffect(async () => {
-    await getUserFirestoreInformation(
-      props.userData.user["uid"],
-      props.userData.user.stsTokenManager["accessToken"]
-    )
-      .then(async (data) => {
-        console.log(data);
-        setUserFirestoreData(data);
-        return data;
-      })
-      .then((data) => {
-        data.teams.map(async (teamId) => {
-          await getTeamFirestoreInformation(
-            teamId,
-            props.userData.user.stsTokenManager["accessToken"]
-          ).then((data) => {
-            data["teamId"] = teamId;
-            setTeamsData((teamsData) => [...teamsData, data]);
-          });
-        });
-      });
-  }, []);
 
   return (
     <div>
       <TextLarge>My Teams</TextLarge>
-      <CreateTeam uid={props.userData.user["uid"]} token={props.userData.user.stsTokenManager["accessToken"]}/>
+      <CreateTeam uid={props.userData.user["uid"]} token={props.userData.user.stsTokenManager["accessToken"]} />
       <br />
-      <UserTeamData userFirestoreData={userFirestoreData} />
+      <UserTeamData uid={props.userData.user["uid"]}  token={props.userData.user.stsTokenManager["accessToken"]} />
     </div>
   );
 }
