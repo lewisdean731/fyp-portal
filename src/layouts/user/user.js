3e923aa736feb2ab5093e4a9b38340abe3a7e1c2
Lewis Dean <lewisdean731@gmail.com>
2021-05-14 13:35:34 +0100

    chore: remove unused function, imports
    [Ticket: FYP-23]

--- src/layouts/user/user.js
+++ src/layouts/user/user.js
@@ -1,22 +1,8 @@
-import { useState, useEffect } from "react";
-import { Row, Col } from "react-bootstrap";
 import TextLarge from "../../components/atoms/text/large/textLarge";
 import UserDetails from "../../components/organisms/ui/user/userDetails/userDetails";
 import UserTeamData from "../../components/organisms/ui/user/userTeamData/userTeamData";
-import { getUserFirestoreInformation } from "../../utils/apiUtil";
 
 function User(props) {
-  const [userFirestoreData, setUserFirestoreData] = useState("");
-
-  useEffect(async () => {
-    await getUserFirestoreInformation(
-      props.userData.user["uid"],
-      props.userData.user.stsTokenManager["accessToken"]
-    ).then((data) => {
-      console.log(data);
-      setUserFirestoreData(data);
-    });
-  }, []);
 
   return (
     <div>
