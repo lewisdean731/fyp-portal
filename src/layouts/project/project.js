4e43053a8bd3ae381dee37dfb6a82a3ca4392d1f
Lewis Dean <lewisdean731@gmail.com>
2021-05-15 21:43:02 +0100

    feat: get project data using ID slug
    [Ticket: FYP-22]

--- src/layouts/project/project.js
+++ src/layouts/project/project.js
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import TextLarge from "../../components/atoms/text/large/textLarge";
+import { getProjectFirestoreInformation } from "../../utils/apiUtil";
 
 function Team(props) {
   const [projectData, setProjectData] = useState("");
   const { projectId } = useParams(); // Gets team ID from URL
 
   useEffect(async () => {
-
+    await getProjectFirestoreInformation(
+      projectId,
+      props.userData.user.stsTokenManager["accessToken"]
+    )
+    .then((data) => {
+      console.log(data)
+      setProjectData(data)
+    })
   }, []);
 
   return (
