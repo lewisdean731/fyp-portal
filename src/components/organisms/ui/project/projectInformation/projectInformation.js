dfe97327011edb6029c0f53c4f311c8234dd1b54
Lewis Dean <lewisdean731@gmail.com>
2021-05-16 00:35:45 +0100

    feat: enable ProjectDetailsDisplay in ProjectInformation
    [Ticket: FYP-22]

--- src/components/organisms/ui/project/projectInformation/projectInformation.js
+++ src/components/organisms/ui/project/projectInformation/projectInformation.js
@@ -1,6 +1,7 @@
 import { Row, Col, Container } from "react-bootstrap";
 import TextMedium from "../../../../atoms/text/medium/textMedium";
 import ProjectDetailsForm from "../../../../molecules/ui/project/projectDetailsForm/projectDetailsForm";
+import ProjectDetailsDisplay from "../../../../molecules/ui/project/projectDetailsDisplay/projectDetailsDisplay";
 import Classes from "./projectInformation.module.scss";
 
 function ProjectInformation(props) {
@@ -9,7 +10,7 @@ function ProjectInformation(props) {
       <TextMedium colour={"grey"}>Project Information</TextMedium>
       <Row className="justify-content-md-center">
         <Col>
-          {/* <ProjectDetailsDisplay projectData={props.projectData} /> */}
+          <ProjectDetailsDisplay projectData={props.projectData} />
           <hr />
           <ProjectDetailsForm projectData={props.projectData} />
         </Col>
