177977fe73521d21655bbdc9c0a2da80696a08c9
Lewis Dean <lewisdean731@gmail.com>
2021-05-16 18:58:50 +0100

    feat: enable createProject in projects layout
    additionally fetch user's teams from API
    
    [Ticket: FYP-22]

--- src/layouts/project/projects.js
+++ src/layouts/project/projects.js
@@ -3,18 +3,27 @@ import { Container } from "react-bootstrap";
 import TextLarge from "../../components/atoms/text/large/textLarge";
 import CreateProject from "../../components/organisms/ui/project/createProject/createProject";
 import UserProjects from "../../components/organisms/ui/project/userProjects/userProjects";
-import { getAllProjectsForUser } from "../../utils/apiUtil";
+import { getAllProjectsForUser, getUserFirestoreInformation } from "../../utils/apiUtil";
 function Teams(props) {
   const [projectsData, setProjectsData] = useState();
+  const [userTeams, setUserTeams] = useState();
 
   useEffect(async () => {
+    // Get all user's projects across teams
     await getAllProjectsForUser(
       props.userData.user["uid"],
       props.userData.user.stsTokenManager["accessToken"]
     )
-    .then((data) => {
+    .then(async (data) => {
       console.log(data)
-      setProjectsData(data.projectsData)
+      // Get user's teams
+      await getUserFirestoreInformation(
+        props.userData.user["uid"],
+        props.userData.user.stsTokenManager["accessToken"]
+      ).then((userData) => {
+        setUserTeams(userData.teams)
+        setProjectsData(data.projectsData)
+      })
     })
   }, []);
 
@@ -22,7 +31,7 @@ function Teams(props) {
     return (
       <Container>
         <TextLarge>My Projects</TextLarge>
-        {/* <CreateProject uid={props.userData.user["uid"]} token={props.userData.user.stsTokenManager["accessToken"]} /> */}
+        <CreateProject uid={props.userData.user["uid"]} token={props.userData.user.stsTokenManager["accessToken"]} userTeams={userTeams} />
         <br />
         <UserProjects projectsData={projectsData} />
       </Container>
