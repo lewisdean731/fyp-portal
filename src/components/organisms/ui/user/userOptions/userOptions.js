d6479395ba8e800ea52428db007f7b16a0c106a6
Lewis Dean <lewisdean731@gmail.com>
2021-05-17 19:19:35 +0100

    feat: implement placeholder user options component
    [Ticket: FYP-28]

---
+++ src/components/organisms/ui/user/userOptions/userOptions.js
@@ -0,0 +1,19 @@
+import { Row, Col, Container } from "react-bootstrap";
+import TextMedium from "../../../../atoms/text/medium/textMedium";
+
+import Classes from "./userOptions.module.scss";
+
+function UserOptions(props) {
+  return (
+    <Container fluid className={Classes.userOptions}>
+      <TextMedium colour={"grey"}>Options</TextMedium>
+      <Row className="justify-content-md-center">
+        <Col>
+          {/* <UserOptionsForm projectData={props.projectData} /> */}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default UserOptions;
