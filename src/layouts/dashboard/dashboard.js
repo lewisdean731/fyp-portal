bbf85ceb3cd2dae40e82b0b868e7a3c099e9cd51
Lewis Dean <lewisdean731@gmail.com>
2021-05-21 02:06:24 +0100

    chore: only render dashboard after API requests finished
    [Ticket: FYP-36]

--- src/layouts/dashboard/dashboard.js
+++ src/layouts/dashboard/dashboard.js
@@ -6,7 +6,7 @@ import DashboardSummary from "../../components/organisms/ui/dashboard/dashboardS
 import { getNotificationsForUser } from "../../utils/apiUtil"
 
 function Dashboard(props) {
-  const [notificationsData, setNotificationsData] = useState([])
+  const [notificationsData, setNotificationsData] = useState()
 
   useEffect(() => {
     async function fetchData() {
@@ -19,16 +19,21 @@ function Dashboard(props) {
     
   }, []);
 
-  return (
-    <div>
-      <TextLarge>Dashboard</TextLarge>
-      <DashboardSummary />
-      <br />
-      <DashboardNotifications notificationsData={notificationsData}/>
-      <br />
-      <DashboardMetrics />
-    </div>
-  );
+  if(notificationsData){
+    return (
+      <div>
+        <TextLarge>Dashboard</TextLarge>
+        <DashboardSummary />
+        <br />
+        <DashboardNotifications notificationsData={notificationsData}/>
+        <br />
+        <DashboardMetrics />
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
+
 }
 
 export default Dashboard;
