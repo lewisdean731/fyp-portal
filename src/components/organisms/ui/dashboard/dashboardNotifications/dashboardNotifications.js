ac07114f3caef7ec3e99b3524b08d34eb74bb9f8
Lewis Dean <lewisdean731@gmail.com>
2021-05-21 02:06:04 +0100

    feat: implement notifications display into dashboardNotifs
    [Ticket: FYP-36]

--- src/components/organisms/ui/dashboard/dashboardNotifications/dashboardNotifications.js
+++ src/components/organisms/ui/dashboard/dashboardNotifications/dashboardNotifications.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import TextMedium from "../../../../atoms/text/medium/textMedium";
+import DashboardNotificationsDisplay from "../../../../molecules/ui/dashboard/dashboardNotificationsDisplay/dashboardNotificationsDisplay";
 import Classes from "./dashboardNotifications.module.scss";
 
-function dashboardNotifications() {
+function dashboardNotifications(props) {
   return (
     <Container className={Classes.dashboardNotifications}>
       <TextMedium colour={"grey"}>Notifications</TextMedium>
       <Row className="justify-content-md-center">
         <Col className={Classes.col}>
-
+          <DashboardNotificationsDisplay notificationsData={props.notificationsData} />
         </Col>
       </Row>
     </Container>
