12aa9bc2a2525c40c5f7ae82ec7527f07f70e4de
Lewis Dean <lewisdean731@gmail.com>
2021-05-22 21:30:33 +0100

    feat: implement metrics to line charts
    [Ticket: FYP-41]

---
+++ src/components/atoms/ui/chart/line/line.js
@@ -0,0 +1,40 @@
+import { React /* useState */ } from "react";
+import { Line } from 'react-chartjs-2';
+import TextMedium from "../../../text/medium/textMedium";
+
+export function LineChart(props) {
+  //const [data, setData] = useState();
+
+  const data = {
+    labels: props.labels,
+    datasets: [
+      {
+        label: '# of Votes',
+        data: props.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  };
+  
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            unit: 'days'
+          }
+        }
+      ]
+    }
+  };
+
+  return (
+    <div>
+      <TextMedium>{props.title}</TextMedium>
+      <Line data={data} options={options} />
+    </div>
+    );
+}
--- src/components/organisms/ui/metrics/dashboardMetrics/metricsDisplay.js
+++ src/components/organisms/ui/metrics/dashboardMetrics/metricsDisplay.js
@@ -1,29 +1,98 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import TextMedium from "../../../../atoms/text/medium/textMedium";
 import { PieChart } from "../../../../atoms/ui/chart/pie/pie";
+import { LineChart } from "../../../../atoms/ui/chart/line/line";
 import Classes from "./metricsDisplay.module.scss";
 
-function MetricsDisplay() {
-  return (
-    <Container className={Classes.metricsDisplay}>
-      <TextMedium colour={"grey"}>Metrics</TextMedium>
-      <Row className="justify-content-md-center">
-        <Col className={Classes.col}>
-          <PieChart />
-        </Col>
-        <Col className={Classes.col}>
-          <PieChart />
-        </Col>
-        <Col className={Classes.col}>
-          <PieChart />
-        </Col>
-        <Col className={Classes.col}>
-          <PieChart />
-        </Col>
-      </Row>
-    </Container>
-  );
+function makeLabels(metricArr){
+  let labels = []
+  metricArr.forEach((obj) => {
+    labels.push(parseInt(obj.timestamp))
+  })
+  return labels;
+}
+
+function makeData(metricArr){
+  let labels = []
+  metricArr.forEach((obj) => {
+    labels.push(parseInt(obj.value))
+  })
+  return labels;
+}
+
+
+function MetricsDisplay(props) {
+  
+  useEffect(() => {}, [props]);
+  
+  function total(){
+    if(props.totalDependencies){
+      return <LineChart
+      title={"Total Dependencies"}
+      label={"# of dependencies"}
+      labels={makeLabels(props.totalDependencies)}
+      data={makeData(props.totalDependencies)}
+    />
+    }
+  }
+  function green(){
+    if(props.greenDependencies){
+      return <LineChart
+      title={"Green Dependencies"}
+      label={"# of dependencies"}
+      labels={makeLabels(props.greenDependencies)}
+      data={makeData(props.greenDependencies)}
+    />
+    }
+  }
+  function yellow(){
+    if(props.yellowDependencies){
+      return <LineChart
+      title={"Yellow Dependencies"}
+      label={"# of dependencies"}
+      labels={makeLabels(props.yellowDependencies)}
+      data={makeData(props.yellowDependencies)}
+    />
+    }
+  }
+  function red(){
+    if(props.redDependencies){
+      return <LineChart
+      title={"Red Dependencies"}
+      label={"# of dependencies"}
+      labels={makeLabels(props.redDependencies)}
+      data={makeData(props.redDependencies)}
+    />
+    }
+  }
+
+  if(props.totalDependencies){
+    return (
+      <Container className={Classes.metricsDisplay}>
+          <TextMedium>Dependencies across Projects</TextMedium>
+          <Row>
+            <Col className={Classes.col}>
+              {total()}
+            </Col>
+            <Col className={Classes.col}>
+              {green()}
+            </Col>
+          </Row>
+          <Row>
+            <Col className={Classes.col}>
+              {yellow()}
+            </Col>
+            <Col className={Classes.col}>
+              {red()}
+            </Col>
+          </Row>
+      </Container>
+    );
+  } 
+  
+  return <p>Loading...</p>
+  
 }
 
 export default MetricsDisplay;
