1351a7c370c1364b4832a91282bd70107576c4de
Lewis Dean <lewisdean731@gmail.com>
2021-05-23 18:57:26 +0100

    feat: get project metrics and pass to dashboardSummary
    [Ticket: FYP-41]

--- src/components/organisms/ui/dashboard/dashboardSummary/dashboardSummary.js
+++ src/components/organisms/ui/dashboard/dashboardSummary/dashboardSummary.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import { getMetrics } from "../../../../../utils/apiUtil";
 import TextMedium from "../../../../atoms/text/medium/textMedium";
 import DashboardItem from "../../../../molecules/ui/dashboard/dashboardItem/dashboardItem";
 
 import Classes from "./dashboardSummary.module.scss";
 
-function DashboardSummary() {
+function DashboardSummary(props) {
+
   return (
     <Container className={Classes.dashboardSummary}>
       <TextMedium colour={"grey"}>At a Glance</TextMedium>
@@ -14,7 +16,7 @@ function DashboardSummary() {
           <DashboardItem
             colour1={"grey"}
             colour2={"black"}
-            text1={10}
+            text1={props.metrics.totalProjects}
             text2={"Total Projects"}
           />
         </Col>
@@ -22,7 +24,7 @@ function DashboardSummary() {
           <DashboardItem
             colour1={"green"}
             colour2={"black"}
-            text1={6}
+            text1={props.metrics.greenProjects}
             text2={"Green Projects"}
           />
         </Col>
@@ -30,7 +32,7 @@ function DashboardSummary() {
           <DashboardItem
             colour1={"yellow"}
             colour2={"black"}
-            text1={3}
+            text1={props.metrics.yellowProjects}
             text2={"Yellow Projects"}
           />
         </Col>
@@ -38,7 +40,7 @@ function DashboardSummary() {
           <DashboardItem
             colour1={"red"}
             colour2={"black"}
-            text1={1}
+            text1={props.metrics.redProjects}
             text2={"Red Projects"}
           />
         </Col>
--- src/layouts/dashboard/dashboard.js
+++ src/layouts/dashboard/dashboard.js
@@ -2,16 +2,22 @@ import { useEffect, useState } from "react";
 import TextLarge from "../../components/atoms/text/large/textLarge";
 import DashboardNotifications from "../../components/organisms/ui/dashboard/dashboardNotifications/dashboardNotifications";
 import DashboardSummary from "../../components/organisms/ui/dashboard/dashboardSummary/dashboardSummary";
-import { getNotificationsForUser } from "../../utils/apiUtil";
+import { getMetrics, getNotificationsForUser } from "../../utils/apiUtil";
 
 function Dashboard(props) {
   const [notificationsData, setNotificationsData] = useState();
+  const [metrics, setMetrics] = useState();
 
   useEffect(() => {
     async function fetchData() {
       await getNotificationsForUser(
         props.userData.user.stsTokenManager["accessToken"]
       ).then((data) => setNotificationsData(data.notificationsData));
+
+      setMetrics(await getMetrics(
+        props.userData.user["uid"],
+        props.userData.user.stsTokenManager["accessToken"]
+      ))
     }
     fetchData();
   }, [props.userData.user.stsTokenManager["accessToken"]]);
@@ -20,7 +26,7 @@ function Dashboard(props) {
     return (
       <div>
         <TextLarge>Dashboard</TextLarge>
-        <DashboardSummary />
+        <DashboardSummary metrics={metrics}/>
         <br />
         <DashboardNotifications
           notificationsData={notificationsData}
