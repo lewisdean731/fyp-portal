e02fcf2e1735af8b64fa4000d3c4d247592971eb
Lewis Dean <lewisdean731@gmail.com>
2021-05-25 00:48:57 +0100

    feat: add page links to navbar
    [Ticket: FYP-63]

--- src/components/organisms/ui/topbar/topbar.js
+++ src/components/organisms/ui/topbar/topbar.js
@@ -4,15 +4,17 @@ export default function Topbar(props) {
   return (
     <>
       <Navbar bg="light" expand="lg" className={Classes.topbar}>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="/dashboard">
           <img className={Classes.logo} src="/logo200.png" alt="logo" />
         </Navbar.Brand>
-        <Navbar.Brand href="/">Monitaur</Navbar.Brand>
+        <Navbar.Brand href="/dashboard">Monitaur</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#">Fake Link</Nav.Link>
-            <Nav.Link href="#">Fake Link</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="/metrics">Metrics</Nav.Link>
+            <Nav.Link href="/teams">Teams</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
