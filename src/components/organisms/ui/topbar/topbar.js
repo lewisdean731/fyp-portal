62ad7a3ae5d90cc2a90c4079f549eb62989b6942
Lewis Dean <lewisdean731@gmail.com>
2021-05-25 00:41:35 +0100

    feat: fix navbar shadow styling

--- src/components/organisms/ui/topbar/topbar.js
+++ src/components/organisms/ui/topbar/topbar.js
@@ -2,35 +2,37 @@ import { Navbar, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
 import Classes from "./topbar.module.scss";
 export default function Topbar(props) {
   return (
-    <Navbar bg="light" expand="lg" className={Classes.topbar}>
-      <Navbar.Brand href="/">
-        <img className={Classes.logo} src="/logo200.png" alt="logo" />
-      </Navbar.Brand>
-      <Navbar.Brand href="/">Monitaur</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#">Fake Link</Nav.Link>
-          <Nav.Link href="#">Fake Link</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        </Form>
-        <Nav className={"mr"}>
-          <NavDropdown
-            title={props.userData.user["displayName"]}
-            alignRight
-            id="basic-nav-dropdown"
-          >
-            <NavDropdown.Item href="/user">Account Settings</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/" onClick={props.signOut}>
-              Sign Out
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+    <>
+      <Navbar bg="light" expand="lg" className={Classes.topbar}>
+        <Navbar.Brand href="/">
+          <img className={Classes.logo} src="/logo200.png" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Brand href="/">Monitaur</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Fake Link</Nav.Link>
+            <Nav.Link href="#">Fake Link</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          </Form>
+          <Nav className={"mr"}>
+            <NavDropdown
+              title={props.userData.user["displayName"]}
+              alignRight
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="/user">Account Settings</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/" onClick={props.signOut}>
+                Sign Out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <div className={Classes.border}></div>
-    </Navbar>
+    </>
   );
 }
